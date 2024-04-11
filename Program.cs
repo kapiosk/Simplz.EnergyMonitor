1c7c0c6916fb27a0d767619a4148a151b2a8a667
AZ <speedsterk@hotmail.com>
2024-04-11 17:37:25 +0300

    Add energy monitor functionality

--- Program.cs
+++ Program.cs
@@ -6,22 +6,49 @@ DotNetEnv.Env.Load();
 var ips = Environment.GetEnvironmentVariable("TAPO_IPS") ?? throw new InvalidOperationException("Environment variable IP is not set.");
 var email = Environment.GetEnvironmentVariable("TAPO_EMAIL") ?? throw new InvalidOperationException("Environment variable EMAIL is not set.");
 var password = Environment.GetEnvironmentVariable("TAPO_PASSWORD") ?? throw new InvalidOperationException("Environment variable PASSWORD is not set.");
+var energyMonitorCron = Environment.GetEnvironmentVariable("ENERGY_MONITOR_CRON");
 
-TapoDeviceClient deviceClient = new();
+TapoDeviceClient deviceClient = new([new TapoConnect.Protocol.KlapDeviceClient()]);
 List<TapoDeviceKey> tapoDeviceKeys = [];
-ips.Split(',').ToList().ForEach(async ip => tapoDeviceKeys.Add(await deviceClient.LoginByIpAsync(ips, email, password)));
-
-PerioticCronTimer perioticCronTimer = new("*/5 * * * * *");
-while (await perioticCronTimer.WaitForNextTickAsync())
+foreach (var ip in ips.Split(','))
 {
+    tapoDeviceKeys.Add(await deviceClient.LoginByIpAsync(ip, email, password));
+}
+
+if (string.IsNullOrEmpty(energyMonitorCron))
+{
+    var items = await ProcessItems(deviceClient, tapoDeviceKeys);
+    foreach (var item in items)
+    {
+        Console.WriteLine($"{item.Nickname} - {item.CurrentPower}w - {item.LocalTime}");
+    }
+}
+else
+{
+    PerioticCronTimer perioticCronTimer = new(energyMonitorCron);
+    while (await perioticCronTimer.WaitForNextTickAsync())
+    {
+        var items = await ProcessItems(deviceClient, tapoDeviceKeys);
+        foreach (var item in items)
+        {
+            Console.WriteLine($"{item.Nickname} - {item.CurrentPower}w - {item.LocalTime}");
+        }
+    }
+}
+
+static async Task<List<TapoDeviceResponse>> ProcessItems(TapoDeviceClient deviceClient, List<TapoDeviceKey> tapoDeviceKeys)
+{
+    List<TapoDeviceResponse> tapoDeviceResponses = [];
     foreach (var tapoDeviceKey in tapoDeviceKeys)
     {
         var energyUsageResult = await deviceClient.GetEnergyUsageAsync(tapoDeviceKey);
-        Console.WriteLine(energyUsageResult.CurrentPower);
         var deviceGetInfoResult = await deviceClient.GetDeviceInfoAsync(tapoDeviceKey);
-        Console.WriteLine(deviceGetInfoResult.Nickname);
+        tapoDeviceResponses.Add(new(deviceGetInfoResult.Nickname, energyUsageResult.CurrentPower / 1000, energyUsageResult.LocalTime));
     }
+    return tapoDeviceResponses;
 }
+
+record TapoDeviceResponse(string Nickname, float CurrentPower, DateTime LocalTime);
 
 class PerioticCronTimer(string cronExpression)
 {
