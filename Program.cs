85f56954854c7ba6d4c9efcf0e10dfc701836c8d
AZ <speedsterk@hotmail.com>
2024-04-12 01:34:19 +0300

    Add cancellation token support to energy monitor functionality

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using Simplz.EnergyMonitor.Utilities;
 
 DotNetEnv.Env.Load();
 
+CancellationTokenSource cts = new();
+cts.CancelAfter(TimeSpan.FromSeconds(5));
+
 var energyMonitorCron = Environment.GetEnvironmentVariable("ENERGY_MONITOR_CRON"); // ?? "0/5 * * * * *"
 
 TapoService tapoService = await TapoService.CreateServiceAsync();
@@ -13,18 +16,18 @@ if (string.IsNullOrEmpty(energyMonitorCron))
     var items = await tapoService.ReadDeviceInfoAsync();
     foreach (var item in items)
     {
-        await influxDBService.WriteDataAsync("energy", item.Nickname, item.CurrentPower, item.LocalTime);
+        await influxDBService.WriteDataAsync("energy", item.Nickname, item.CurrentPower, item.LocalTime, cts.Token);
     }
 }
 else
 {
     PerioticCronTimer perioticCronTimer = new(energyMonitorCron);
-    while (await perioticCronTimer.WaitForNextTickAsync())
+    while (await perioticCronTimer.WaitForNextTickAsync(cts.Token))
     {
         var items = await tapoService.ReadDeviceInfoAsync();
         foreach (var item in items)
         {
-            await influxDBService.WriteDataAsync("energy", item.Nickname, item.CurrentPower, item.LocalTime);
+            await influxDBService.WriteDataAsync("energy", item.Nickname, item.CurrentPower, item.LocalTime, cts.Token);
         }
     }
 }
